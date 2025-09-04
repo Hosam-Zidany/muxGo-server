ed2147766c97ba4f9decc6bcd048e8a7f31991cb
Hosam Zidany <hosam.zidany.1@gmail.com>
2025-09-05 00:04:08 +0300

    fix error

--- main.go
+++ main.go
@@ -23,7 +23,7 @@ var (
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", RootHandler)
-	mux.HandleFunc("/user/{id}", userHandler)
+	mux.HandleFunc("/user/", userHandler)
 	fmt.Println("Server is Running on :8080")
 	log.Fatal(http.ListenAndServe(":8080", mux))
 }
@@ -33,22 +33,23 @@ func RootHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func userHandler(w http.ResponseWriter, r *http.Request) {
+	idStr := r.URL.Path[len("/user/"):]
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid ID", http.StatusBadRequest)
+		return
+	}
 	switch r.Method {
 	case http.MethodGet:
-		getUser(w, r)
+		getUser(w, r, id)
 	case http.MethodPost:
-		addUser(w, r)
-		// case http.MethodDelete:
-		// deleteUser(w, r)
+		addUser(w, r, id)
+	case http.MethodDelete:
+		deleteUser(w, r, id)
 	}
 }
 
-func getUser(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.PathValue("id"))
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
+func getUser(w http.ResponseWriter, r *http.Request, id int) {
 	smux.RLock()
 	user, ok := Memo[id]
 	smux.RUnlock()
@@ -62,16 +63,11 @@ func getUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.Write(ret)
 	w.WriteHeader(http.StatusOK)
+	w.Write(ret)
 }
 
-func addUser(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.PathValue("id"))
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	}
+func addUser(w http.ResponseWriter, r *http.Request, id int) {
 	var user User
 	err2 := json.NewDecoder(r.Body).Decode(&user)
 	if err2 != nil {
@@ -85,4 +81,14 @@ func addUser(w http.ResponseWriter, r *http.Request) {
 	smux.Lock()
 	Memo[id] = user
 	smux.Unlock()
+}
+
+func deleteUser(w http.ResponseWriter, r *http.Request, id int) {
+	smux.RLock()
+	_, ok := Memo[id]
+	smux.RUnlock()
+	if ok {
+		delete(Memo, id)
+	}
+	return
 }
